5437fd58e3eaf7ade204a38a0dfab2585a1fdbc2
McClymont <kieraninnewzealand@hotmail.com>
2018-02-20 19:52:07 +1300

    Updated window.location details + adapted the response to absence of access_token

--- src/App.js
+++ src/App.js
@@ -115,7 +115,8 @@ class App extends Component {
   componentDidMount() {
     let parsed = queryString.parse(window.location.search)
     let accessToken = parsed.access_token
-
+    if (!accessToken)
+      return;
     fetch('https://api.spotify.com/v1/me', {
       headers: {"Authorization": 'Bearer ' + accessToken}
     }).then(response => response.json())
@@ -146,7 +147,6 @@ class App extends Component {
 
     return (
       <div className="App">
-      {/* Here the comp is asking whether there is before executing */}
         {this.state.user ?
           <div>
             <h1>
@@ -161,17 +161,14 @@ class App extends Component {
                 )
               }
             </div>
-          </div> : <button onClick={() =>
-            window.location =
-            window.location.includes('localhost')
-            ? "https://localhost:8888/login"
-            : "https://better-playlists-backend-kie.herokuapp.com/login"
-          }
-            style={{'padding': '20px', 'margin': '20px'}}>Login to Spotify</button>
+          </div> : <button onClick={() => {window.location =
+            window.location.href.includes('localhost')
+            ? "http://localhost:8888/login"
+            : "https://better-playlists-backend-kie.herokuapp.com/login"}
+          } style={{'padding': '20px', 'margin': '20px'}}>Login to Spotify</button>
         }
       </div>
-
-    );
+    )
   }
 }
 
